d4fe2f12ddf07bef6a4f1021d88af329efc6fa31
Oceanic Navigator <b@oceanic.im>
2026-09-18 17:23:46 +0100

    Create DownSubcommand.php
    Added Down command

---
+++ src/Subcommands/DownSubcommand.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace InterconnectIt\LaravelLocalServer\Subcommands;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class DestroySubcommand extends Subcommand
+{
+    const COMMAND = 'docker-compose down --remove-orphans';
+
+    public function __invoke(InputInterface $input, OutputInterface $output): int
+    {
+        $output->writeln('<info>Stopping and clearing containers...</>');
+
+        return $this->runProcess(static::COMMAND);
+    }
+}
